e41042e450ceb65573c1e4f60e87edb9429732d4
Hardik Shah <techpotatohardik@Hardiks-MacBook-Pro.local>
2026-07-29 14:43:12 +0530

    fix: deployment failure

--- apps/cms/src/app/(frontend)/[slug]/page.tsx
+++ apps/cms/src/app/(frontend)/[slug]/page.tsx
@@ -14,27 +14,25 @@ import PageClient from './page.client'
 import { LivePreviewListener } from '@/components/LivePreviewListener'
 
 export async function generateStaticParams() {
-  const payload = await getPayload({ config: configPromise })
-  const pages = await payload.find({
-    collection: 'pages',
-    draft: false,
-    limit: 1000,
-    overrideAccess: false,
-    pagination: false,
-    select: {
-      slug: true,
-    },
-  })
-
-  const params = pages.docs
-    ?.filter((doc) => {
-      return doc.slug !== 'home'
-    })
-    .map(({ slug }) => {
-      return { slug }
+  try {
+    const payload = await getPayload({ config: configPromise })
+    const pages = await payload.find({
+      collection: 'pages',
+      draft: false,
+      limit: 1000,
+      overrideAccess: false,
+      pagination: false,
+      select: {
+        slug: true,
+      },
     })
 
-  return params
+    return pages.docs
+      ?.filter((doc) => doc.slug !== 'home')
+      .map(({ slug }) => ({ slug }))
+  } catch {
+    return []
+  }
 }
 
 type Args = {
